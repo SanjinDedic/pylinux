b5e36001e40dcf7d20b0182b16fa70edf7f13e71
Sanjin Dedic <ozrobotix@gmail.com>
2022-12-04 01:16:13 -0500

    adding a placeholder for advanced gdrive downloader

---
+++ gdrive_downloads/gdrive_downloader.py
@@ -0,0 +1,3 @@
+#this script will download files and folders from google drive
+#It will perform some basic url cleaning to work with different types of links
+#The users will also have access to a help function
