5b06b604ae1cb4f51a91f684201264c71715e3e9
DawoodLaiq <86498931+DawoodLaiq@users.noreply.github.com>
2022-12-06 01:23:40 +0500

    added code to gdrive and git downloader

--- gdrive_downloads/gdrive_downloader.py
+++ gdrive_downloads/gdrive_downloader.py
@@ -1,3 +1,33 @@
 #this script will download files and folders from google drive
 #It will perform some basic url cleaning to work with different types of links
 #The users will also have access to a help function
+
+import wget,gdown
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-gfile', '--download_google_file', help="Downloads a Google File")
+    parser.add_argument('-gfolder', '--download_google_folder', help="Downloads a Google Folder")
+    
+    args = parser.parse_args()
+
+    if args.download_google_file:
+        url = args.download_google_file
+        file_id = url.split('/')[-2]
+
+        prefix = 'https://drive.google.com/uc?/export=download&id='
+        wget.download(prefix+file_id)
+        print("File downloaded")
+
+    if args.download_google_folder:
+        url = args.download_google_folder
+        if url.split('/')[-1] == '?usp=sharing':
+            url= url.replace('?usp=sharing','')
+        gdown.download_folder(url)
+        print("Folder downloaded")
+
+
+if __name__=="__main__":
+    main()
--- git_downloads/git_downloader.py
+++ git_downloads/git_downloader.py
@@ -1,2 +1,28 @@
 #this script integrated downloading files and folders
 #it also incluses a -help
+
+import urllib.request,os
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-gitfile', '--download_github_file', help="Downloads a Github File")
+    parser.add_argument('-gitfolder', '--download_github_folder', help="Downloads a Github Folder")
+    
+    args = parser.parse_args()
+
+    if args.download_github_file:
+        url = args.download_github_file
+        url = url.replace("/blob","")
+        url = url.replace("github.com","raw.githubusercontent.com")
+        file_name = url.split('/')[-1]
+        urllib.request.urlretrieve(url, file_name)
+        print("File downloaded")
+
+    if args.download_github_folder:
+        os.system('gitdir '+args.download_github_folder)
+        print("Folder downloaded")
+
+if __name__=="__main__":
+    main()
