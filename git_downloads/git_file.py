3c18e884cad9e6ceacd6bb95843432d49ecc6b38
DawoodLaiq <86498931+DawoodLaiq@users.noreply.github.com>
2022-12-06 12:21:50 +0500

    Change in gfile for string cleaning

--- git_downloads/git_file.py
+++ git_downloads/git_file.py
@@ -1,6 +1,8 @@
 import urllib.request, sys 
 
 url = sys.argv[1]
+url = url.replace("/blob","")
+url = url.replace("github.com","raw.githubusercontent.com")
 file_name = url.split('/')[-1]
 
 urllib.request.urlretrieve(url, file_name)
