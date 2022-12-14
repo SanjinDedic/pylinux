86ffa06c2806bdf163bb787cc9c0e2ff65e1cd4c
Sanjin84 <ozrobotix@gmail.com>
2022-12-14 17:48:05 +1100

    Update system_search.py

--- file_system_search/system_search.py
+++ file_system_search/system_search.py
@@ -1,6 +1,5 @@
 #This script goes through every sub folder from an identified folder
 #It searches for a string within popular document types: office, pdf, archives
-#It can also search via built in linux commands like find
 
 import os,re
 from PyPDF2 import PdfFileReader
