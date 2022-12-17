de86659f49b5ddae4a47b436989cdeab1657cb40
Sanjin84 <ozrobotix@gmail.com>
2022-12-17 17:51:49 +1100

    Update pdf_flag_finder.py

--- file_system_search/pdf_flag_finder.py
+++ file_system_search/pdf_flag_finder.py
@@ -2,7 +2,7 @@ from PyPDF2 import PdfFileReader
 import sys,os,re
 
 file = sys.argv[1]
-text= sys.argv[2]+".+}"
+text= sys.argv[2]+".+}" //".+}" . means any character + 1 or more occurences, ends with }
 if file.endswith(".pdf"):
     with open(file, 'rb') as f:
         pdf_file=PdfFileReader(f)
